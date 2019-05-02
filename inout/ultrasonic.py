89c7b84ba775dc264490e8fe7bc7ee081f0cd753
bloodymosquito <bloodymosquito>
2019-05-03 01:20:36 +0200

    Modify ultrasonic.py
    Add comments.

--- inout/ultrasonic.py
+++ inout/ultrasonic.py
@@ -3,7 +3,10 @@
 
 """
 Module to use a HC-SR04 ultrasonic sensor with RPi.
+
+If not run on a RPi, the behaviour is simulated and there is no real, hardware change such as pin reading.
 """
+
 
 
 import random
@@ -21,9 +24,20 @@ else:
 
 
 class UltrasonicSensor:
-    """TODO"""
+    """Class to define that a specific GPIO pin is an ultrasonic sensor.
+
+    Methods to measure the distance once or get the median on several measures.
+    """
+
     def __init__(self, trig=18, echo=24, temperature=20):
-        """TODO"""
+        """Initialization function.
+        Set the trigger pin as an output and the echo pin as an input.
+
+        Args:
+            trig: The pin number of the trigger pin.
+            echo: The pin number of the echo pin.
+            temperature: The temperature, to estimate the speed of sound.
+        """
 
         self._trig = trig
         self._echo = echo
@@ -49,18 +63,21 @@ class UltrasonicSensor:
         return self._speed
 
     def measure(self):
-        """TODO"""
-        # set Trigger to HIGH for 10 microseconds
+        """Do one measure with the ultrasonic sensor.
 
-        # WARNING here
+        WARNING: if not run on a RPi, this function return a random value for illustration purpose.
+        """
+
         # When not running on a raspberry pi, we just return a random value.
         if not RPI:
             return random.uniform(0, 3)
 
+        # Set Trigger to HIGH for 10 microseconds
         GPIO.output(self.trig, GPIO.HIGH)
         time.sleep(0.00001)
         GPIO.output(self.trig, GPIO.LOW)
 
+        # Wait for the echo pulse
         counter = 0
         while GPIO.input(self.echo) == GPIO.LOW:
             start_time = time.time()
@@ -71,14 +88,20 @@ class UltrasonicSensor:
         while GPIO.input(self.echo) == GPIO.HIGH:
             stop_time = time.time()
 
-        # get time and deduce distance
+        # Get time and deduce distance
         time_elapsed = stop_time - start_time       # in seconds
         distance = time_elapsed * self.speed / 2    # in meters
 
         return distance
 
     def median_measure(self, rep=11, pause=0.1):
-        """TODO"""
+        """Perform several measures and return the median.
+
+        Args:
+            rep: Number of measures.
+            pause: Time between measures.
+        """
+
         measures = []
         for _ in range(rep):
             # Wait between measures
